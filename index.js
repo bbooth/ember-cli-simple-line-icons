b027436ac4703e06a2ad18d1056be733b661783c
Bradley Booth <bradley.booth@gmail.com>
2015-02-04 00:27:58 -0700

    Fixed font destination directory

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = {
     this.app = app;
     this._super.included(app);
 
-    app.import("vendor/simple-line-icons/css/simple-line-icons.css");
-    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.eot");
-    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.svg");
-    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.ttf");
-    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.woff");
+    app.import("vendor/simple-line-icons/css/simple-line-icons.css", { destDir: "fonts" });
+    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.eot", { destDir: "fonts" });
+    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.svg", { destDir: "fonts" });
+    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.ttf", { destDir: "fonts" });
+    app.import("vendor/simple-line-icons/fonts/Simple-Line-Icons.woff", { destDir: "fonts" });
   }
 };
